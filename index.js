105e16c4f9fea7ca8a9e40916ddb603079a5be85
justin <justinmwathi32@gmail.com>
2024-04-02 22:45:33 +0300

    Fetch-lab completed

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
-}
+const url="https://anapioficeandfire.com/api/books"
+return (fetch(url)
+.then((response)=>response.json())
+.then((data)=>renderBooks(data)) 
+)}
 
 function renderBooks(books) {
   const main = document.querySelector('main');
